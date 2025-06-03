3b25d5b2738f30ccf4c995ad3608564ef5746753
bokarios <abubakr.sg@gmail.com>
2025-06-03 15:42:14 +0400

    Add domain models for Event, Ticket, TicketType, QrCode, TicketValidation, and User

---
+++ src/main/java/com/devsato/tickets/domain/Ticket.java
@@ -0,0 +1,79 @@
+package com.devsato.tickets.domain;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false, updatable = false)
+  private String id;
+
+  @Column(name = "status", nullable = false)
+  private TicketStatusEnum status;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "ticket_type_id")
+  private TicketType ticketType;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "purchaser_id")
+  private User purchaser;
+
+  @OneToMany(mappedBy = "tickets", cascade = CascadeType.ALL)
+  private List<TicketValidation> validations = new ArrayList<>();
+
+  @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+  private List<QrCode> qrCodes = new ArrayList<>();
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false, nullable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Ticket ticket = (Ticket) o;
+    return Objects.equals(id, ticket.id) && status == ticket.status && Objects.equals(createdAt, ticket.createdAt)
+        && Objects.equals(updatedAt, ticket.updatedAt);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, status, createdAt, updatedAt);
+  }
+
+}
