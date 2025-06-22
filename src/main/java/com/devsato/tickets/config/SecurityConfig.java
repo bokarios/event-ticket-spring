e72ae96ad1070bdae8ead6e1e98d99aa7fb58cc1
bokarios <abubakr.sg@gmail.com>
2025-06-22 17:06:24 +0400

    Implement JWT authentication converter and update security configuration to use it for role-based access control

--- src/main/java/com/devsato/tickets/config/SecurityConfig.java
+++ src/main/java/com/devsato/tickets/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.devsato.tickets.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
@@ -15,14 +14,17 @@ import com.devsato.tickets.filters.UserProvisioningFilter;
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter)
+  public SecurityFilterChain filterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter,
+      JwtAuthenticationConverter jwtAuthenticationConverter)
       throws Exception {
     http.authorizeHttpRequests(
         authorize -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
+            .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
             .anyRequest().authenticated())
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+            jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
         .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
 
     return http.build();
